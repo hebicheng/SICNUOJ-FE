f92b7e0fafb4ce433e1ed64c258e3b121c3078a2
hebicheng <heb1c@qq.com>
2021-12-09 10:36:31 +0800

    更新后台dashboard

--- src/pages/admin/views/general/Dashboard.vue
+++ src/pages/admin/views/general/Dashboard.vue
@@ -30,6 +30,20 @@
           </el-form>
         </div>
       </el-card>
+      <div class="info-container" v-if="isSuperAdmin">
+        <el-card>
+          <info-card color="#909399" icon="el-icon-fa-users" message="Total Users" iconSize="30px" class="info-item"
+                      :value="infoData.user_count"></info-card>
+          <info-card color="#67C23A" icon="el-icon-fa-list" message="Today Submissions" class="info-item"
+                      :value="infoData.today_submission_count"></info-card>
+          <info-card color="#409EFF" icon="el-icon-fa-trophy" message="Recent Contests" class="info-item"
+                      :value="infoData.recent_contest_count"></info-card>
+        </el-card>
+      </div>
+      
+    </el-col>
+
+    <el-col :md="14" :lg="16" v-if="isSuperAdmin">
       <panel :title="$t('m.System_Overview')" v-if="isSuperAdmin">
         <p>{{$t('m.DashBoardJudge_Server')}}:  {{infoData.judge_server_count}}</p>
         <p>{{$t('m.HTTPS_Status')}}:
@@ -48,46 +62,76 @@
           </el-tag>
         </p>
       </panel>
-    </el-col>
 
-    <el-col :md="14" :lg="16" v-if="isSuperAdmin">
-      <div class="info-container">
-        <info-card color="#909399" icon="el-icon-fa-users" message="Total Users" iconSize="30px" class="info-item"
-                   :value="infoData.user_count"></info-card>
-        <info-card color="#67C23A" icon="el-icon-fa-list" message="Today Submissions" class="info-item"
-                   :value="infoData.today_submission_count"></info-card>
-        <info-card color="#409EFF" icon="el-icon-fa-trophy" message="Recent Contests" class="info-item"
-                   :value="infoData.recent_contest_count"></info-card>
+      <div class="view">
+        <Panel :title="$t('m.Judge_Server_Token')">
+          <code>{{ token }}</code>
+        </Panel>
+        <Panel :title="$t('m.Judge_Server_Info')">
+          <el-table
+            :data="servers"
+            :default-expand-all="true"
+            border>
+            <el-table-column
+              type="expand">
+              <template slot-scope="props">
+                <p>{{$t('m.IP')}}:
+                  <el-tag type="success">{{ props.row.ip }}</el-tag>&nbsp;&nbsp;
+                  {{$t('m.Judger_Version')}}:
+                  <el-tag type="success">{{ props.row.judger_version }}</el-tag>
+                </p>
+                <p>{{$t('m.Service_URL')}}: <code>{{ props.row.service_url }}</code></p>
+                <p>{{$t('m.Last_Heartbeat')}}: {{ props.row.last_heartbeat | localtime}}</p>
+                <p>{{$t('m.Create_Time')}}: {{ props.row.create_time | localtime }}</p>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="status"
+              label="Status">
+              <template slot-scope="scope">
+                <el-tag
+                  :type="scope.row.status === 'normal' ? 'success' : 'danger'">
+                  {{ scope.row.status === 'normal' ? 'Normal' : 'Abnormal' }}
+                </el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="hostname"
+              label="Hostname">
+            </el-table-column>
+            <el-table-column
+              prop="task_number"
+              label="Task Number">
+            </el-table-column>
+            <el-table-column
+              prop="cpu_core"
+              label="CPU Core">
+            </el-table-column>
+            <el-table-column
+              prop="cpu_usage"
+              label="CPU Usage">
+              <template slot-scope="scope">{{ scope.row.cpu_usage }}%</template>
+            </el-table-column>
+            <el-table-column
+              prop="memory_usage"
+              label="Memory Usage">
+              <template slot-scope="scope">{{ scope.row.memory_usage }}%</template>
+            </el-table-column>
+            <el-table-column label="Disabled">
+              <template slot-scope="{row}">
+                <el-switch v-model="row.is_disabled" @change="handleDisabledSwitch(row.id, row.is_disabled)"></el-switch>
+              </template>
+            </el-table-column>
+            <el-table-column
+              fixed="right"
+              label="Options">
+              <template slot-scope="scope">
+                <icon-btn name="Delete" icon="trash" @click.native="deleteJudgeServer(scope.row.hostname)"></icon-btn>
+              </template>
+            </el-table-column>
+          </el-table>
+        </Panel>
       </div>
-      <panel style="margin-top: 5px">
-        <span slot="title" v-loading="loadingReleases">Release Notes
-        <el-popover placement="right" trigger="hover">
-          <i slot="reference" class="el-icon-fa-question-circle import-user-icon"></i>
-          <p>Please upgrade to the latest version to enjoy the new features. </p>
-          <p>Reference: <a href="http://docs.onlinejudge.me/#/onlinejudge/guide/upgrade" target="_blank">
-          http://docs.onlinejudge.me/#/onlinejudge/guide/upgrade</a>
-          </p>
-        </el-popover>
-        </span>
-
-        <el-collapse v-model="activeNames" v-for="(release, index) of releases" :key="'release' + index">
-          <el-collapse-item :name="index+1">
-            <template slot="title">
-              <div v-if="release.new_version">{{release.title}}
-                <el-tag size="mini" type="success">New Version</el-tag>
-              </div>
-              <span v-else>{{release.title}}</span>
-            </template>
-            <p>Level: {{release.level}}</p>
-            <p>Details: </p>
-            <div class="release-body">
-              <ul v-for="detail in release.details" :key="detail">
-                <li v-html="detail"></li>
-              </ul>
-            </div>
-          </el-collapse-item>
-        </el-collapse>
-      </panel>
     </el-col>
   </el-row>
 </template>
@@ -116,10 +160,19 @@
         activeNames: [1],
         session: {},
         loadingReleases: true,
-        releases: []
+        releases: [],
+        servers: [],
+        token: '',
+        intervalId: -1
       }
     },
     mounted () {
+      if (this.isSuperAdmin) {
+        this.refreshJudgeServerList()
+        this.intervalId = setInterval(() => {
+          this.refreshJudgeServerList()
+        }, 5000)
+      }
       api.getDashboardInfo().then(resp => {
         this.infoData = resp.data.data
       }, () => {
@@ -154,6 +207,31 @@
           })[0]
         }
         this.session = session
+      },
+      refreshJudgeServerList () {
+        api.getJudgeServer().then(res => {
+          this.servers = res.data.data.servers
+          this.token = res.data.data.token
+        })
+      },
+      deleteJudgeServer (hostname) {
+        this.$confirm('If you delete this judge server, it can\'t be used until next heartbeat', 'Warning', {
+          confirmButtonText: 'Delete',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          api.deleteJudgeServer(hostname).then(res =>
+            this.refreshJudgeServerList()
+          )
+        }).catch(() => {
+        })
+      },
+      handleDisabledSwitch (id, value) {
+        let data = {
+          id,
+          is_disabled: value
+        }
+        api.updateJudgeServer(data).catch(() => {})
       }
     },
     computed: {
@@ -179,6 +257,10 @@
         let b = browserDetector(this.session.user_agent)
         return b.os ? b.os : 'Unknown'
       }
+    },
+    beforeRouteLeave (to, from, next) {
+      clearInterval(this.intervalId)
+      next()
     }
   }
 </script>
@@ -213,7 +295,7 @@
     flex-wrap: wrap;
     .info-item {
       flex: 1 0 auto;
-      min-width: 200px;
+      min-width: 400px;
       margin-bottom: 10px;
     }
   }
